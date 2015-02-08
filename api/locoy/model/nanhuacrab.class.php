bf59af7da0cef27bcd015363993e92635d5875e5
nanhuacrab <nanhuacrab@hotmail.com>
2015-02-08 20:41:50 +0800

    update company url

--- api/locoy/model/nanhuacrab.class.php
+++ api/locoy/model/nanhuacrab.class.php
@@ -2,6 +2,26 @@
 
 class nanhuacrab_controller extends common{
 	
+	// company original_page_url is null
+	function fix2_action(){
+		include("locoy_config.php");
+		if($locoyinfo['locoy_online']!=1){
+			echo 4;die;
+		}
+		if($locoyinfo['locoy_key']!=trim($_GET['key'])){
+			echo 5;die;
+		}
+
+		echo "fix2";
+		
+		$companys=$this->obj->DB_select_all("company","`original_page_url` IS NULL OR `original_page_url` = '' ORDER BY `uid` DESC LIMIT 0, 1000");
+		foreach( $companys as $company ) {
+			$uid = $company["uid"];
+			$job = $this->obj->DB_select_once("company_job","`uid`=".$uid." AND `page_url` IS NOT NULL AND `page_url` != ''");
+			echo "<tr><td>".$uid."</td><td><a href='".$job["page_url"]."'></a></td></tr>";
+		}
+	}
+	
 	function fix_action(){
 		include("locoy_config.php");
 		if($locoyinfo['locoy_online']!=1){
